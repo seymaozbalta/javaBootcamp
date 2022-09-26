5d77118fff51ac90db51b7db00f86fcef18bdefa
seymaozbalta <seymaozbalta@gmail.com>
2022-09-26 15:08:09 +0300

    encapsulation and overloading works

--- javaHomeworks/src/classesWithAttributes/Main.java
+++ javaHomeworks/src/classesWithAttributes/Main.java
@@ -3,18 +3,25 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product=new Product();
-		product.name="Laptop";
-		product.id=1;
-		product.description="Asus Laptop";
-		product.price=5000;
-		product.stockAmount=3;
+		Product product= new Product(1,"Laptop","Asus Laptop",3000,2,"Siyah");
+		
+		/*yapıcıdan önceki field tanımlama
+		 * Product product=new Product();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
+		product.setRenk("mor");*/
+		
 		
 		ProductManager productManager=new ProductManager();
 		productManager.Add(product);
 		//System.out.println(product.name);
 		//örneğin bir sitedeki bunların texbox olduklarını farzedelim
 		//bunları alırken kullanabiliriz classları
+		//productManager.Add2(1, "null", "null", 20, 200, null);
+		System.out.println(product.getKod());
 		
 
 	}
--- javaHomeworks/src/classesWithAttributes/ProductManager.java
+++ javaHomeworks/src/classesWithAttributes/ProductManager.java
@@ -3,6 +3,10 @@ package classesWithAttributes;
 public class ProductManager {
 	public void Add(Product product) {
 		//JDBS
-		System.out.println("Ürün eklendi "+product.name);
+		System.out.println("Ürün eklendi "+product.getName());
+	}
+	
+	public void Add2(int id, String name, String description, int stockAmount, double price, String yeniOzellik) {
+		 
 	}
 }
